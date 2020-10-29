22b18172eba0f1463a277c33ea0d62b5c24ac67c
Life <Life@123>
2020-10-29 22:45:57 +0800

    第一版

---
+++ MVC5HomeWork/Controllers/BankaccountController.cs
@@ -0,0 +1,21 @@
+﻿using MVC5HomeWork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace MVC5HomeWork.Controllers
+{
+    public class BankaccountController : Controller
+    {
+        // GET: Bankaccount
+        private 客戶資料Entities db= new 客戶資料Entities();
+        public ActionResult Index()
+        {
+            var data = db.客戶銀行資訊.Include(c => c.客戶資料);
+            return View(data.ToList()) ;
+        }
+    }
+}
